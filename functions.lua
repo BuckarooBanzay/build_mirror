44421f58018efbe014cd0b942565eac1a5c8ca52
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-07-12 11:25:11 +0200

    more param2 mappings

--- functions.lua
+++ functions.lua
@@ -18,55 +18,92 @@ function build_mirror.mirror_pos(pos, mirror_pos, axis)
     return mirrored_pos
 end
 
-local wallmounted = {
-    [90]  = {0, 1, 5, 4, 2, 3, 0, 0},
-    [180] = {0, 1, 3, 2, 5, 4, 0, 0},
-    [270] = {0, 1, 4, 5, 3, 2, 0, 0}
-}
-
-local facedir = {
-    [90]  = { 1,  2,  3,  0, 13, 14, 15, 12, 17, 18, 19, 16,
-              9, 10, 11,  8,  5,  6,  7,  4, 23, 20, 21, 22},
-    [180] = { 2,  3,  0,  1, 10, 11,  8,  9,  6,  7,  4,  5,
-             18, 19, 16, 17, 14, 15, 12, 13, 22, 23, 20, 21},
-    [270] = { 3,  0,  1,  2, 19, 16, 17, 18, 15, 12, 13, 14,
-              7,  4,  5,  6, 11,  8,  9, 10, 21, 22, 23, 20}
-}
-
-function build_mirror.rotate_node(node, axis, degrees)
-    if axis ~= "y" then
-        -- can't rotate any other axis than y at the moment
-        return node
+local function invert_map(m)
+    local inv = {}
+    for k, v in pairs(m) do
+        inv[v] = k
     end
+    return inv
+end
 
+-- x- to x+
+local param2_x_mapping = {
+    [1] = 3,
+    [3] = 1,
+    [5] = 7,
+    [9] = 11,
+    [16] = 11,
+    [18] = 14,
+    [23] = 21,
+    [21] = 23
+}
+
+-- x- to x+
+local param2_x_mapping_inv = invert_map(param2_x_mapping)
+
+-- y- to y+
+local param2_y_mapping = {
+    [20] = 0,
+    [23] = 1,
+    [22] = 2,
+    [21] = 3
+}
+
+-- y+ to y-
+local param2_y_mapping_inv = invert_map(param2_y_mapping)
+
+-- z- to z+
+local param2_z_mapping = {
+    [0] = 2,
+    [2] = 0,
+    [9] = 5,
+    [11] = 14,
+    [12] = 14,
+    [16] = 18,
+    [20] = 22,
+    [22] = 20
+}
+
+-- z- to z+
+local param2_z_mapping_inv = invert_map(param2_z_mapping)
+
+function build_mirror.rotate_node(node, axis_direction)
     local def = minetest.registered_nodes[node.name]
     if not def then
         return node
     end
     local paramtype2 = def.paramtype2
 
-    local rotation_catalog, orient
-    if paramtype2 == "wallmounted" or paramtype2 == "colorwallmounted" then
-        rotation_catalog = wallmounted
-        orient = node.param2 % 8
-    elseif paramtype2 == "facedir" or paramtype2 == "colorfacedir" then
-        rotation_catalog = facedir
-        orient = node.param2 % 32
-    else
+    if paramtype2 ~= "facedir" and paramtype2 ~= "colorfacedir" then
         -- can't rotate this
         return node
     end
 
-    local substitution = rotation_catalog[degrees]
-    if not substitution then
-        return node
+    print("rotate_node param2=" .. node.param2 .. " axis_direction=" .. axis_direction)
+
+    local param2
+
+    if axis_direction == "x+" then
+        param2 = param2_x_mapping[node.param2]
+    elseif axis_direction == "x-" then
+        param2 = param2_x_mapping_inv[node.param2]
+    elseif axis_direction == "y+" then
+        param2 = param2_y_mapping[node.param2]
+    elseif axis_direction == "y-" then
+        param2 = param2_y_mapping_inv[node.param2]
+    elseif axis_direction == "z+" then
+        param2 = param2_z_mapping[node.param2]
+    elseif axis_direction == "z-" then
+        param2 = param2_z_mapping_inv[node.param2]
+    else
+        error("not a valid axis-direction: " .. axis_direction)
     end
 
-    local param2 = node.param2 - orient + substitution[orient + 1]
+    print("rotate_node(II) param2=" .. (param2 or "nil"))
 
     return {
         name = node.name,
-        param2 = param2
+        param2 = param2 or node.param2
     }
 end
 
@@ -82,45 +119,75 @@ function build_mirror.get_mirrored_positions(pos, node, mirror_pos, mirrored_axe
     local node_list = {}
 
     if mirrored_axes.x then
-        local mirrored_node = build_mirror.rotate_node(node, "y", 180)
+        local axis_direction = (mirror_pos.x - pos.x < 0) and "x-" or "x+"
+        local mirrored_node = build_mirror.rotate_node(node, axis_direction)
+
         table.insert(pos_list, build_mirror.mirror_pos(pos, mirror_pos, "x"))
         table.insert(node_list, mirrored_node)
     end
 
     if mirrored_axes.y then
+        local axis_direction = (mirror_pos.y - pos.y < 0) and "y-" or "y+"
+        local mirrored_node = build_mirror.rotate_node(node, axis_direction)
+
         table.insert(pos_list, build_mirror.mirror_pos(pos, mirror_pos, "y"))
-        table.insert(node_list, node)
+        table.insert(node_list, mirrored_node)
     end
 
     if mirrored_axes.z then
-        local mirrored_node = build_mirror.rotate_node(node, "y", 180)
+        local axis_direction = (mirror_pos.z - pos.z < 0) and "z-" or "z+"
+        local mirrored_node = build_mirror.rotate_node(node, axis_direction)
+
         table.insert(pos_list, build_mirror.mirror_pos(pos, mirror_pos, "z"))
         table.insert(node_list, mirrored_node)
     end
 
     if mirrored_axes.x and mirrored_axes.y then
+        local x_axis_direction = (mirror_pos.x - pos.x < 0) and "x-" or "x+"
+        local y_axis_direction = (mirror_pos.y - pos.y < 0) and "y-" or "y+"
+
         local mirrored_pos = build_mirror.mirror_pos(pos, mirror_pos, "x")
+        local mirrored_node = build_mirror.rotate_node(node, x_axis_direction)
+
         table.insert(pos_list, build_mirror.mirror_pos(mirrored_pos, mirror_pos, "y"))
-        table.insert(node_list, node)
+        table.insert(node_list, build_mirror.rotate_node(mirrored_node, y_axis_direction))
     end
 
     if mirrored_axes.x and mirrored_axes.z then
+        local x_axis_direction = (mirror_pos.x - pos.x < 0) and "x-" or "x+"
+        local z_axis_direction = (mirror_pos.z - pos.z < 0) and "z-" or "z+"
+
         local mirrored_pos = build_mirror.mirror_pos(pos, mirror_pos, "x")
+        local mirrored_node = build_mirror.rotate_node(node, x_axis_direction)
+
         table.insert(pos_list, build_mirror.mirror_pos(mirrored_pos, mirror_pos, "z"))
-        table.insert(node_list, node)
+        table.insert(node_list, build_mirror.rotate_node(mirrored_node, z_axis_direction))
     end
 
     if mirrored_axes.y and mirrored_axes.z then
+        local y_axis_direction = (mirror_pos.y - pos.y < 0) and "y-" or "y+"
+        local z_axis_direction = (mirror_pos.z - pos.z < 0) and "z-" or "z+"
+
         local mirrored_pos = build_mirror.mirror_pos(pos, mirror_pos, "y")
+        local mirrored_node = build_mirror.rotate_node(node, y_axis_direction)
+
         table.insert(pos_list, build_mirror.mirror_pos(mirrored_pos, mirror_pos, "z"))
-        table.insert(node_list, node)
+        table.insert(node_list, build_mirror.rotate_node(mirrored_node, z_axis_direction))
     end
 
     if mirrored_axes.x and mirrored_axes.y and mirrored_axes.z then
+        local x_axis_direction = (mirror_pos.x - pos.x < 0) and "x-" or "x+"
+        local y_axis_direction = (mirror_pos.y - pos.y < 0) and "y-" or "y+"
+        local z_axis_direction = (mirror_pos.z - pos.z < 0) and "z-" or "z+"
+
         local mirrored_pos = build_mirror.mirror_pos(pos, mirror_pos, "x")
         mirrored_pos = build_mirror.mirror_pos(mirrored_pos, mirror_pos, "y")
+
+        local mirrored_node = build_mirror.rotate_node(node, x_axis_direction)
+        mirrored_node = build_mirror.rotate_node(mirrored_node, y_axis_direction)
+
         table.insert(pos_list, build_mirror.mirror_pos(mirrored_pos, mirror_pos, "z"))
-        table.insert(node_list, node)
+        table.insert(node_list, build_mirror.rotate_node(mirrored_node, z_axis_direction))
     end
 
     return pos_list, node_list
